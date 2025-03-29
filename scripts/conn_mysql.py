b6a2571a6438d53f6c8fbf4d308769007d86e7f9
Diego Paraguai <diego.paraguai.dp@gmail.com>
2025-03-29 08:23:11 -0300

    Implementa a criação de banco de dados e tabela no MySQL usando os nomes das colunas do CSV

--- scripts/conn_mysql.py
+++ scripts/conn_mysql.py
@@ -17,21 +17,21 @@ class ConnMySQL():
                     user=os.getenv('USER'),
                     password=os.getenv('PASSWORD')
                 )
-            print("Conexão bem-sucedida ao MySQL.")
+            print("\nConexão bem-sucedida ao MySQL.")
             return connection
-        except mysql.Error as err:
-            print(f"Erro ao conectar ao MySQL: {err}")
+        except mysql.Error as e:
+            print(f"\nErro ao conectar ao MySQL:\n{e}")
             
     def __conn_cursor(self):
         if self.__connection:
             return self.__connection.cursor()
         else:
-            print("Nenhuma conexão ativa.")
+            print("\nNenhuma conexão ativa.")
 
     def close(self):
         if self.__connection:
             self.cursor.close()
             self.__connection.close()
-            print("Conexão com o MySQL encerrada.")
+            print("\nConexão com o MySQL encerrada.")
         else:
-            print("Nenhuma conexão ativa para encerrar.")
+            print("\nNenhuma conexão ativa para encerrar.")
--- scripts/up_mysql.py
+++ scripts/up_mysql.py
@@ -5,12 +5,49 @@ from scripts.conn_mysql import ConnMySQL
 class UpToMySQL():
     def __init__(self):
         self.ms = ConnMySQL()
+        self.db_name = 'db_mobiles'
+        self.tb_name = 'mobiles'
         self.kaggle_path = 'data/from_kaggle/Mobiles Dataset (2025).csv'
         self.df = None
         self.extract_data_from_csv()
+        self.create_db()
+        self.create_tb()
 
     def extract_data_from_csv(self):
-        self.df = pd.read_csv(
-            self.kaggle_path, delimiter=',', encoding="windows-1252")
+        try:
+            self.df = pd.read_csv(
+                self.kaggle_path, delimiter=',', encoding="windows-1252")
+            print(
+                f'\nAlguns dados Extraidos de {self.kaggle_path}:\n{self.df.sample(5)}')
+        except Exception as e:
+            print(
+                f'Não foi possivel extrar os dados de {self.kaggle_path}:\n{e}')
+
+    def create_db(self):
+        try:
+            self.ms.cursor.execute(
+                f'CREATE DATABASE IF NOT EXISTS {self.db_name}')
+            self.ms.cursor.execute(f'USE {self.db_name}')
+        except Exception as e:
+            print(f'Não foi possivel criar a base dados {self.db_name}:\n{e}')
+
+    def create_tb(self):
+        headers = self.df.columns.to_list()
+        fields = ''
+
+        for field in headers:
+            fields += f'''{(field)
+                                .lower()
+                                .replace(" ", "_")
+                                .replace("(", "")
+                                .replace(")", "")} VARCHAR(100),'''
+        fields = fields.rstrip(',')
+
         print(
-            f'Alguns dados Extraidos de {self.kaggle_path}: \n{self.df.sample(5)}')
+            f'\nLista de Colunas a Serem Criadas no MySQL na tabela {self.tb_name}:\n{fields}')
+        try:
+            self.ms.cursor.execute(f'DROP TABLE IF EXISTS {self.tb_name}')
+            tb_execute = f'CREATE TABLE {self.tb_name} ({fields})'
+            self.ms.cursor.execute(tb_execute)
+        except Exception as e:
+            print(f'Não foi Possivel criar a Tabela {self.tb_name}:\n{e}')
