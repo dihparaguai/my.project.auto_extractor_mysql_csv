9ad0562fb10a9d7452258a02bd9e57bd6a54c9fc
Diego Paraguai <diego.paraguai.dp@gmail.com>
2025-03-30 12:11:44 -0300

    Adiciona funcionalidade para padronizar a coluna battery_capacity e criar um CSV com os dados transformados

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from scripts.up_mysql import UpToMySQL
+from scripts.down_mysql import DownFromMySQL
 
 if __name__ == "__main__":
     # Executa a classe UpToMySQL para criar a base de dados e a tabela no MySQL, e inserir os dados do CSV na tabela criada.
     UpToMySQL()
-
+    
+    # Executa a classe DownFromMySQL para extrair os dados do MySQL, transformar os dados e criar um CSV com os dados transformados.
+    DownFromMySQL()
 
--- scripts/down_mysql.py
+++ scripts/down_mysql.py
@@ -5,14 +5,15 @@ from scripts.conn_mysql import ConnMySQL
 # Objetivo: Extrair os dados do MySQL, transformar os dados e criar um CSV com os dados transformados.
 class DownFromMySQL():
     def __init__(self):
-        self.mysql_path = 'data/from_mysql/mobiles_dataset_2025.csv'
         self.ms = ConnMySQL()
+        self.mysql_path = 'data/from_mysql/mobiles_dataset_2025.csv'
         self.db_name = 'db_mobiles'
         self.tb_name = 'mobiles'
         self.columns = ['company_name', 'model_name', 'battery_capacity', 'ram', 'screen_size']
         self.df = None
         self.extract_data_from_mysql()
         self.standardize_column()
+        self.create_csv()
         self.ms.close()
 
     # Forma a string com os nomes das colunas a serem extraidas do MySQL e retorna a string formatada        
@@ -34,8 +35,19 @@ class DownFromMySQL():
             self.df = pd.DataFrame([data for data in self.ms.cursor], columns=self.columns)
             print(f'\nAlguns dados Extraidos de {self.tb_name}:\n{self.df.sample(5).sort_index()}')
         except Exception as e:
-            print(f'Não foi possivel extrar os dados de {self.tb_name}:\n{e}')
-        
+            print(f'\nNão foi possivel extrar os dados de {self.tb_name}:\n{e}')
+
+    # Padroniza os dados da coluna battery_capacity, removendo as vírgulas e espaços em branco
     def standardize_column(self):
         self.df['battery_capacity'] = self.df['battery_capacity'].str.replace(',', '').replace(' ', '')
         print(f'\nDados padronizados da coluna battery_capacity:\n{self.df["battery_capacity"].sample(5).sort_index()}')
+
+    # Cria o arquivo CSV com os dados extraidos do MySQL e padronizados
+    # O nome do arquivo é formado pelo nome do arquivo original + data e hora atual
+    def create_csv(self):
+        try:
+            self.mysql_path = self.mysql_path.replace('.csv', f'{time.strftime('%m%d_%H%M%S')}.csv')
+            self.df.to_csv(self.mysql_path, index=False)
+            print(f'\nArquivo CSV criado com sucesso em {self.mysql_path}')
+        except Exception as e:
+            print(f'\nNão foi possivel criar o arquivo CSV:\n{e}')
